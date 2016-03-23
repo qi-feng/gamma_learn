f455c51763169ee18d5bfa608d5f96c7e4dfc5ef
qi-feng <sely520@gmail.com>
2016-03-23 18:25:04 -0400

    added reading vegas on events

--- raw_image_learn/get_VEGASOnEvts.py
+++ raw_image_learn/get_VEGASOnEvts.py
@@ -26,7 +26,7 @@ def getVEGASonEvtNum(f, runnum=72045, thetasq=0.01):
 def getVEGASonOneRun(f, runnum=72045, thetasq=0.01):
     getVEGASonEvtNum(f, runnum=runnum, thetasq=thetasq)
     train_x1, train_y1, test_x1, test_y1 = get_raw_features_on(infile=str(runnum)+'.txt',
-                    in_pickle_onfile = 'on'+str(runnum)+'.pkl', test_ratio=0.3, random_state=1234,
+                    in_pickle_onfile = 'VEGASon'+str(runnum)+'.pkl', test_ratio=0.3, random_state=1234,
                     dump=False, out_trainx=str(runnum)+'_raw_trainx_on.pkl', out_trainy=str(runnum)+'_raw_trainy_on.pkl',
                     out_testx=str(runnum)+'_raw_testx_on.pkl', out_testy=str(runnum)+'_raw_testy_on.pkl')
     return train_x1, train_y1, test_x1, test_y1
