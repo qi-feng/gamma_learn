f7f1367ed1e9a1bdf66c89ee198bec7250f294ce
qi-feng <sely520@gmail.com>
2016-05-15 17:04:33 -0400

    Added PBH test code

--- pbh.py
+++ pbh.py
@@ -121,6 +121,19 @@ class Pbh(object):
 
     def psf_func(self, theta2, psf_width, N=100):
         return 1.71*N/2./np.pi/psf_width**2/np.cosh(np.sqrt(theta2)/psf_width)
+        #equivalently:
+        #return (stats.hypsecant.pdf(np.sqrt(theta2s)/psf_width)*1.71/2./psf_width**2)
+
+    def psf_cdf(self, psf_width, fov=1.75):
+        """
+        :param psf_width: same as psf_func
+        :param fov: given so that we calculate cdf from 0 to fov
+        :return:
+        """
+        theta2s=np.arange(0, fov, 0.01)
+        cdf = np.cumsum(self.psf_func(theta2s, psf_width, N=1))
+        cdf = cdf/np.max(cdf)
+        return cdf
 
     #use hard coded width table from the hyperbolic secant function
     def get_psf(self, E=0.1, EL=80):
@@ -149,6 +162,37 @@ class Pbh(object):
             dists[i]=self.get_angular_distance(coord, cent_coord)
         return dists
 
+    def gen_one_random_coords(self, cent_coord, theta):
+        """
+        *** Here use small angle approx, as it is only a sanity check ***
+        :return a pair of uniformly random RA and Dec at theta deg away from the cent_coord
+        """
+        _phi = np.random.random()*np.pi*2.
+        _ra = cent_coord[0]+np.sin(_phi)*theta
+        _dec = cent_coord[1]+np.cos(_phi)*theta
+        return np.array([_ra, _dec])
+
+    def gen_one_random_theta(self, psf_width, prob="psf", fov=1.75):
+        """
+        :prob can be either "psf" that uses the hyper-sec function, or "uniform", or "gauss"
+        """
+        if prob.lower()=="psf" or prob=="hypersec" or prob=="hyper secant":
+            #_rand_theta = np.random.random()*fov
+            _rand_test_cdf = np.random.random()
+            _theta2s=np.arange(0, fov, 0.01)
+            _cdf = np.cumsum(self.psf_func(_theta2s, psf_width, N=1))
+            _cdf = _cdf/np.max(_cdf)
+            #y_interp = np.interp(x_interp, x, y)
+            _theta = np.interp(_rand_test_cdf, _cdf, _theta2s)
+            return _theta
+        elif prob.lower()=="uniform" or prob=="uni":
+            return np.random.random()*fov
+        #gauss may have a caveat as this is not important
+        elif prob.lower()=="gauss" or prob=="norm" or prob.lower()=="gaussian":
+            return abs(np.random.normal())*fov
+        else:
+            return "Input prob value not supported"
+
     def centroid_log_likelihood(self, cent_coord, coords, psfs):
         """
         returns ll=-2*ln(L)
@@ -164,10 +208,16 @@ class Pbh(object):
     def minimize_centroid_ll(self, coords, psfs):
         init_centroid = np.mean(coords, axis=0)
         results = minimize(self.centroid_log_likelihood, init_centroid, args=(coords, psfs), method='L-BFGS-B')
-        return results.x
+        centroid = results.x
+        ll_centroid = self.centroid_log_likelihood(centroid, coords, psfs)
+        return centroid, ll_centroid
 
-    def search_angular_window(self):
-        centroid = self.minimize_centroid_ll(self.coords, self.photon_df.psfs)
+    def search_angular_window(self, coords, psfs):
+        centroid, ll_centroid = self.minimize_centroid_ll(coords, psfs)
+        if ll_centroid < self.ll_cut:
+            return centroid, ll_centroid
+        else:
+            return False, False
 
     #def search_time_window
 
@@ -179,14 +229,14 @@ class Pbh(object):
             ax=plt.subplot(111)
         ax.plot(theta2s, self.psf_func(theta2s, psf_width, N=N), 'r--')
         ax.plot(theta2s, const_, 'b:')
-        ax.plot(theta2s, self.psf_func(theta2s, 0.1)+const_, 'k-')
+        ax.plot(theta2s, self.psf_func(theta2s, psf_width)+const_, 'k-')
         if ylog:
             ax.set_yscale('log')
         ax.set_xlabel(r'$\theta^2$ (deg$^2$)')
         ax.set_ylabel("Count")
         return ax
 
-    def plot_skymap(self, coords, Es, ELs, ax=None, color='r'):
+    def plot_skymap(self, coords, Es, ELs, ax=None, color='r', fov_center=None, fov=1.75, fov_color='gray'):
         if ax is None:
             fig=plt.figure(figsize=(5,5))
             ax=plt.subplot(111)
@@ -194,16 +244,84 @@ class Pbh(object):
         for coor, E_, EL_ in zip(coords, Es, ELs):
             circ=plt.Circle(coor, radius=self.get_psf(E_, EL_), color=color, fill=False)
             ax.add_patch(circ)
+        if fov is not None and fov_center is not None:
+            circ_fov=plt.Circle(fov_center, radius=fov, color=fov_color, fill=False)
+            ax.add_patch(circ_fov)
 
         ax.set_xlabel('RA')
         ax.set_ylabel("Dec")
         return ax
 
 
+class powerlaw:
+    # Class can calculate a power-law pdf, cdf from x_min to x_max,
+    # always normalized so that the integral pdf is 1
+    # (so is the diff in cdf between x_max and xmin)
+    ############################################################################
+    #                       Initialization of the object.                      #
+    ############################################################################
+    def __init__(self, a, x_min, x_max):
+        """
+        :param a: photon index, dN/dE = E^a
+        :param x_min: e lo
+        :param x_max: e hi
+        :return:
+        """
+        self.a = a
+        self.x_min = x_min
+        self.x_max = x_max
+
+    def pdf(self, x):
+        if self.a==-1:
+            return -1
+        self.pdf_norm = (self.a+1)/(self.x_max**(self.a+1.0) - self.x_min**(self.a+1.0))
+        return self.pdf_norm*x**self.a
+
+    def cdf(self, x):
+        if self.a==-1:
+            return -1
+        self.cdf_norm = 1./(self.x_max**(self.a+1.0) - self.x_min**(self.a+1.0))
+        return self.cdf_norm*(x**(self.a+1.0) - self.x_min**(self.a+1.0))
+
+    def ppf(self, q):
+        if self.a==-1:
+            return -1
+        norm = (self.x_max**(self.a+1.0) - self.x_min**(self.a+1.0))
+        return (q*norm*1.0+self.x_min**(self.a+1.0))**(1.0/(self.a+1.0))
+
+    def random(self, n):
+        r_uniform = np.random.random_sample(n)
+        return self.ppf(r_uniform)
+
+
+def test_psf_func():
+    pbh = Pbh()
+    fov_center = np.array([180., 30.0])
+    fov = 1.75
+
+    #spec sim:
+    index = -2.5
+    E_min = 0.08
+    E_max = 50.0
+    #Burst size to visualize
+    Nsim = 10
+    EL = 15
+    pl_nu = powerlaw(index, E_min, E_max)
+    rand_Es =  pl_nu.random(Nsim)
+    rand_coords = np.zeros((Nsim, 2))
+    for i in range(Nsim):
+        psf_width = pbh.get_psf(rand_Es[i], EL)
+        rand_theta = pbh.gen_one_random_theta(psf_width)
+        rand_coords[i,:] = pbh.gen_one_random_coords(fov_center, rand_theta)
+    pbh.plot_skymap(rand_coords,rand_Es, [EL]*Nsim, fov_center=fov_center)
+    plt.show()
+    return pbh
+
 def test1():
     pbh = Pbh()
-    ras = np.random.random(size=10)*2.0+180.
-    decs = np.random.random(size=10)*1.5+30.
+    fov_center = np.array([180., 30.0])
+    ras = np.random.random(size=10)*2.0+fov_center[0]
+    decs = np.random.random(size=10)*1.5+fov_center[1]
     coords = np.concatenate([ras.reshape(10,1), decs.reshape(10,1)], axis=1)
     psfs = np.ones(10)*0.1
     centroid = pbh.minimize_centroid_ll(coords,psfs)
@@ -212,7 +330,7 @@ def test1():
     print centroid.reshape(1,2)[:,0], centroid.reshape(1,2)[:,1]
 
     ax = pbh.plot_skymap(coords, [0.1]*10, [0.2]*10)
-    pbh.plot_skymap(centroid.reshape(1,2), [0.1], [0.2], ax=ax, color='b')
+    pbh.plot_skymap(centroid.reshape(1,2), [0.1], [0.2], ax=ax, color='b', fov_center=fov_center)
     plt.show()
 
 def test2():
@@ -222,4 +340,4 @@ def test2():
     return pbh
 
 if __name__ == "__main__":
-    pbh = test2()
+    pbh = test_psf_func()
